0af2a25d2d31cd6840e3695fd9be0e8b533db042
Ayush Tiwari <ayushtiwari@Ayushs-MacBook-Pro.local>
2022-09-23 04:26:21 +0530

    compelte uplers assement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment, useRef } from "react";
-import "./App.css";
+
 var interval = setInterval(() => {}, 0);
 function App() {
   const [time, setTimer] = useState("00:00");
@@ -9,7 +9,6 @@ function App() {
     let minutes = minutesInput.current.value;
     let seconds = secondsInput.current.value;
     interval = setInterval(() => {
-      console.log("called after every seconds", seconds);
       if (seconds > 0 && minutes >= 0) {
         setTimer(
           (minutes > 9 ? minutes : "0" + minutes) +
@@ -26,7 +25,6 @@ function App() {
           setTimer("00:00");
         }
       } else if (minutes < 0) {
-        console.log("minutes is less than 0");
         //stops the clock
         clearInterval(interval);
       }
@@ -52,8 +50,21 @@ function App() {
       >
         START
       </button>
-      <button>PAUSE / RESUME</button>
-      <button>RESET</button>
+      <button
+        onClick={() => {
+          window.clearInterval(interval);
+        }}
+      >
+        PAUSE / RESUME
+      </button>
+      <button
+        onClick={() => {
+          window.clearInterval(interval);
+          setTimer("00:00");
+        }}
+      >
+        RESET
+      </button>
       <h1 data-testid="running-clock">{time}</h1>
     </Fragment>
   );
